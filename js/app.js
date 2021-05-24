79b0674ca9186971e269f26e7f4f94fbddaa3b65
Ayrat Gimranov <gimranov45@gmail.com>
2021-05-24 13:24:21 -0700

    added map into body of game.html and a sample JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-'use strict'
+'use strict';
+
+let pathElem = document.getElementsByTagName('path');
+
+for (let i = 0; i < pathElem.length; i++){
+
+  pathElem[i].addEventListener('click', handleEvent);
+
+  function handleEvent(event){
+
+    if (pathElem[i].hasAttribute('class')){
+      pathElem[i].removeAttribute('class');
+    } else {
+      pathElem[i].setAttribute('class', 'countryRed');
+    }
+
+  }
+}
